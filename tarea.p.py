a1fae4611895d8563850480f47346ca5070551f0
erickleyva <72729127+erickleyva@users.noreply.github.com>
2020-10-14 03:09:47 -0500

    Add files via upload

---
+++ tarea.p.py
@@ -0,0 +1,29 @@
+def suma( x, y):
+    return (x+y)
+
+def resta(x,y):
+    return(x-y)
+
+def division(x,y):
+    resultado=x/y
+    return(resultado)
+
+def multiplicacion(x,y):
+    resultado= x*y
+    return(resultado)
+
+
+numero1 =  5
+numero2 =  10
+print ( suma(numero1,numero2))
+print ( resta(numero2,numero1))
+print (multiplicacion(numero1,numero2))
+print (division(numero2,numero1))
+
+numero3= 10
+numero4=32
+
+print ( suma(numero3,numero4))
+print ( resta(numero4,numero3))
+print (multiplicacion(numero3,numero4))
+print (division(numero4,numero3))
